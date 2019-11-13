e8581178332bd92ae85c464047f9eb6d21d8d543
anasanwar2 <57671285+anasanwar2@users.noreply.github.com>
2019-11-13 15:26:06 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("643813432772132867")
+setInterval(function() {
+channel.send(`Anas THE King Here thx you for listen Anas THE King Here thx you for listen Anas THE King Here thx you for listen`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
